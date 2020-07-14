ea3cc744b8de8996479063f51233b696d59147f6
Soh, Justin Y (UG - Computer Science) <ys00715@surrey.ac.uk>
2020-07-14 21:25:31 +0100

    Pagin, sorting and filtering (section 6)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -12,6 +12,7 @@ using API.Dtos;
 using AutoMapper;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -43,14 +44,16 @@ namespace API.Controllers
         [HttpGet]
         // Asynchronously returns a list of products.
         // CALLS THE REPOSITORY METHODS
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+            var totalItems = await _productsRepo.CountAsync(countSpec);
             var products = await _productsRepo.ListAsync(spec);
 
-            if (products == null) return NotFound(new ApiResponse(404));
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
         }
 
         // Http Get method with the id as a parameter
